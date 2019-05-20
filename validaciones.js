1bbc3ddd855e6292a1660529396a80f8c6d5d591
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2019-05-20 02:22:20 -0500

    Metodos ACTUALIZADO
    Metodos:
    Nombre
    Apellido
    Fecha
    Direecion
    Telefono

--- validaciones.js
+++ validaciones.js
@@ -25,9 +25,8 @@ function validarCedula() {
     }
 }
 
-function soloNumeros(e) {
+function soloNumeros(e, cad) {
     var key = window.Event ? e.which : e.keyCode
-    var cad = document.getElementById("cedula").value;
     return ((key >= 48) && (key <= 57) && (cad.length + 1 <= 10) || (key == 8))
 }
 
@@ -44,13 +43,71 @@ function soloLetras(e) {
             break;
         }
     }
-
     if (letras.indexOf(tecla) == -1 && !tecla_especial) {
         return false;
     }
 }
 
-
-function estas() {
-    alert('estas tipiand')
+function dosNombres(txt, id) {
+    palabras = txt.split(' ');
+    if (palabras.length == 2) {
+        p1 = palabras[0].trim();
+        p2 = palabras[1].trim();
+        if (p1 != '' && p2.length >= 3) {
+            document.getElementById(id).checked = true;
+        } else {
+            document.getElementById(id).checked = false;
+        }
+    } else {
+        document.getElementById(id).checked = false;
+    }
 }
+
+function telefonoVal(txt, id) {
+    if (txt.length >= 10) {
+        document.getElementById(id).checked = true;
+    } else {
+        document.getElementById(id).checked = false;
+    }
+}
+
+function validarFecha(fecha, id) {
+    if (fecha.length == 10) {
+        f = fecha.split('/');
+        var Anio = f[2]
+        var Mes = f[1]
+        var Dia = f[0]
+        var VFecha = new Date(Anio, Mes, Dia);
+        if ((VFecha.getFullYear() == Anio) && (VFecha.getMonth() == Mes) && (VFecha.getDate() == Dia)) {
+            document.getElementById(id).checked = true;
+        }
+        else {
+            document.getElementById(id).checked = false;
+        }
+    } else {
+        document.getElementById(id).checked = false;
+    }
+}
+
+function fecha10(txt, e) {
+    var key = window.Event ? e.which : e.keyCode
+    if (txt.length >= 10) {
+        if (key != 8) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+
+function valDir(txt, id) {
+    if (txt.length >= 4) {
+        document.getElementById(id).checked = true;
+    } else {
+        document.getElementById(id).checked = false;
+    }
+}
+
+
