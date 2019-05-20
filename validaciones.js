796a8494c2fcc19f25ef194ec9548fee0a8d4f07
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2019-05-20 00:37:34 -0500

    Cedula arreglada

--- validaciones.js
+++ validaciones.js
@@ -19,6 +19,7 @@ function validarCedula() {
         } else {
             document.getElementById("chCedula").checked = false;
         }
+
     } else {
         document.getElementById("chCedula").checked = false;
     }
@@ -26,13 +27,29 @@ function validarCedula() {
 
 function soloNumeros(e) {
     var key = window.Event ? e.which : e.keyCode
-    var ced = document.getElementById("cedula").value;
-    if (key >= 48 && key <= 57 && ced.length <= 9 || key == 8) {
-        return true;
-    } else {
+    var cad = document.getElementById("cedula").value;
+    return ((key >= 48) && (key <= 57) && (cad.length + 1 <= 10) || (key == 8))
+}
+
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
         return false;
     }
 }
+
 
 function estas() {
     alert('estas tipiand')
